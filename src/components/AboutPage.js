828695283c176583e9f9e81771959a7d11db9a5b
RomanAlekseev <r.alekseev@andersenlab.com>
2020-02-20 02:06:03 +0300

    html small changes

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -12,5 +12,15 @@ export const aboutPage = new AboutPage({
               <ul>
                 <li><a href="#menu">Menu</a></li> 
                 <li><a href="#items">Items</a></li> 
-              </ul>`
+              </ul>
+              <hr/>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+                ducimus aliquam, molestiae quaerat ad facere blanditiis hic error at
+                exercitationem animi sed nihil odio! Perferendis dicta, voluptatum vitae,
+                veniam omnis veritatis accusantium quasi nostrum sit aliquid
+                necessitatibus officia inventore porro labore odio quia alias expedita
+                quibusdam nam, cupiditate temporibus quo.
+             </p>
+              `
 });
--- src/components/ItemsPage.js
+++ src/components/ItemsPage.js
@@ -6,11 +6,29 @@ class ItemsPage extends ViewFrameWorkComponent {
   }
 }
 
+const data = {
+  items: {
+    1: { name: "item1", quantity: "1", price: "20" },
+    2: { name: "item2", quantity: "5", price: "5" },
+    3: { name: "item3", quantity: "3", price: "30" }
+  },
+  total: 3
+};
+
+const listArr = Object.entries(data.items).map(item => {
+  return `<li>name: ${item[1].name},<br/> quantity: ${item[1].quantity},<br/> price: ${item[1].price} </li>`;
+});
+
 export const itemsPage = new ItemsPage({
   entryClass: "home",
   template: `<div>Items Component</div>
               <ul>
-              <li><a href="#menu">Menu</a></li> 
-              <li><a href="#about">About</a></li>  
-              </ul>`
+                <li><a href="#menu">Menu</a></li> 
+                <li><a href="#about">About</a></li>  
+              </ul>
+              <hr/>
+              <ul>        
+               ${listArr}
+              </ul>
+              `
 });
