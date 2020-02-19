b013e06f82413a8988b88d8c93c47aafbba1df26
RomanAlekseev <r.alekseev@andersenlab.com>
2020-02-19 13:04:19 +0300

    Set initial configuration for webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+
+  entry: "./index.js",
+
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "public")
+  },
+
+  devtool: "eval",
+
+  devServer: {
+    port: 4200
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]
+};
