580142102b2eed63c944686f14c9f77465cdf2c9
RomanAlekseev <r.alekseev@andersenlab.com>
2020-02-20 13:54:21 +0300

    disable CORS

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+var cors = require("cors");
+
 const app = express();
 
 const items = require("./data");
+const port = 3000;
+
+app.use("*", cors());
 
 app.get("/items", (req, res) => {
   res.send(items);
 });
 
-app.listen(3000);
+app.listen(port, () => console.log("Server is up on port " + port));
