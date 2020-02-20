af675378c8e4b3951bc2e0d1ff2faf344f2be2f0
RomanAlekseev <r.alekseev@andersenlab.com>
2020-02-20 13:38:27 +0300

    add data.json

--- server/server.js
+++ server/server.js
@@ -1,14 +1,7 @@
 const express = require("express");
 const app = express();
 
-const items = {
-  items: {
-    1: { name: "item1", quantity: "1", price: "20" },
-    2: { name: "item2", quantity: "5", price: "5" },
-    3: { name: "item3", quantity: "3", price: "30" }
-  },
-  total: 3
-};
+const items = require("./data");
 
 app.get("/items", (req, res) => {
   res.send(items);
