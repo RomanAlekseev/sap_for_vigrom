2463db3862f986d83b5c146b68ca3653945a272d
RomanAlekseev <r.alekseev@andersenlab.com>
2020-02-21 10:35:04 +0300

    styles refactoring

--- src/templates/itemsPageTemplate.js
+++ src/templates/itemsPageTemplate.js
@@ -2,19 +2,19 @@ import { formatArray } from "./templateUtils";
 
 const template = ({ list, total }) => {
   return `<button class="goBackButton">GoBack</button>
-  <div class="col-lg-d-flex align-items-center">
+  <div class="col-lg-d-flex mx-auto ">
   <h2 class="mr-5">Items</h2>
-  <ul class="list-group list-group-horizontal-xl">
+  <ul class="list-group list-group-horizontal-lg ">
     <li class="list-group-item"><a href="#menu">Menu</a></li>
     <li class="list-group-item"><a href="#items">Items</a></li>
     <li class="list-group-item"><a href="#about">About</a></li> 
   </ul>
   </div>
   <hr/>
-  <ul class="mt-3">        
+  <ul class="mt-3 d-flex justify-content-around items">        
      ${formatArray(list)}
     </ul>
-    <span>Total: ${total}</span>
+    <div class="mb-3"><b>Total: ${total}</b></div>
     `;
 };
 
