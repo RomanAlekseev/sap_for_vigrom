bcda9eb18e99180c81d8d90211b28317e06dbcea
RomanAlekseev <r.alekseev@andersenlab.com>
2020-02-22 23:31:23 +0300

    move framework into the assets

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   resolve: {
     alias: {
-      framework: path.resolve(__dirname, "src/framework/"),
+      framework: path.resolve(__dirname, "public/assets/framework/"),
       templates: path.resolve(__dirname, "src/templates/")
     }
   },
