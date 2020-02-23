f3bd5f045e8955752b7541116edc5d105cdebe68
RomanAlekseev <r.alekseev@andersenlab.com>
2020-02-23 17:33:17 +0300

    Rework menu

--- src/templates/common/navigationTemplate.js
+++ src/templates/common/navigationTemplate.js
@@ -1,9 +1,18 @@
 const template = current => {
   const html = `
          <a class="nav-item nav-link" href="/">Home </a>
-         <a class="nav-item nav-link" href="#menu">Menu</a>
          <a class="nav-item nav-link" href="#items">Items</a>
          <a class="nav-item nav-link" href="#about">About</a>
+         <div class="dropdown position-relative">
+           <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+             Menu
+           </a>
+         
+           <div class="dropdown-menu position-absolute" aria-labelledby="dropdownMenuLink">
+             <a class="dropdown-item" href="#items">Items</a>
+             <a class="dropdown-item" href="#about">About</a>
+           </div>
+        </div>
           `;
   // Find active navigation element and add class="active"
   function findActiveElement(hash) {
