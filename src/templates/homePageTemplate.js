f2e5bd8f9bc283bc498c1e1ded86d3155d499380
RomanAlekseev <r.alekseev@andersenlab.com>
2020-02-24 22:10:00 +0300

    Change home page

--- src/templates/homePageTemplate.js
+++ src/templates/homePageTemplate.js
@@ -1,13 +1,14 @@
 const template = () => `
 <div class="col-lg-d-flex align-items-center mt-4">
-  <h2 class="mr-5">Home</h2>
+  <h2 class="mr-5 text-center">Test SPA</h2>
 </div>
-<p class="mt-5 text-center">
-  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
-  perspiciatis id consectetur qui asperiores reiciendis quod alias culpa? Ab
-  sunt exercitationem eius. Quod fugiat odit voluptate modi sit dolorum
-  praesentium dolorem sunt. Dolore, ut corrupti.
-</p>
+<ul class="d-flex flex-column align-items-center">
+  <li>По условию тестовое задание было выполнено без view-фреймворков</li>
+  <li>Основано на хеш навигации</li>
+  <li>История передвижения не более 10-и элементов</li>
+  <li>Имеется кнопка назад</li>
+  <li>Использованы стили Bootstrap</li>
+</ul>
 `;
 
 export default template;
