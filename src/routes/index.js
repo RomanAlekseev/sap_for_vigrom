5ef8b7254a4e56fcb96022d28bb20252ab4fc436
RomanAlekseev <r.alekseev@andersenlab.com>
2020-02-24 22:45:03 +0300

    add animation for logo 'rotate logo'

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,19 +3,19 @@ import { navigation } from "../components/common/navigation";
 
 export const appRoutes = {
   "/": () =>
-    import(/* webpackMode: lazy */ "../components/pages/homePage/").then(
+    import(/* webpackMode: "lazy" */ "../components/pages/homePage/").then(
       page => page.default
     ),
   about: () =>
-    import(/* webpackMode: lazy */ "../components/pages/aboutPage/").then(
+    import(/* webpackMode: "lazy" */ "../components/pages/aboutPage/").then(
       page => page.default
     ),
   items: () =>
-    import(/* webpackMode: lazy */ "../components/pages/itemsPage/").then(
+    import(/* webpackMode: "lazy" */ "../components/pages/itemsPage/").then(
       page => page.default
     ),
   notFound: () =>
-    import(/* webpackMode: lazy */ "../components/pages/notFoundPage/").then(
+    import(/* webpackMode: "lazy" */ "../components/pages/notFoundPage/").then(
       page => page.default
     ),
   common: [navigation, goBackButton]
